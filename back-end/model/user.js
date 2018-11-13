9cb1dc09a48e518608625467ee8febabe572dc10
Jamie Burns <t7buja00@students.oamk.fi>
2018-11-13 11:33:01 +0200

    Added User Routes
    +nodemon

--- back-end/model/user.js
+++ back-end/model/user.js
@@ -1,9 +1,32 @@
-const db = require('../database');
+const db = require("../database");
 const users = {
     getAllUsers(callback) {
-        return db.query('SELECT * FROM users', callback);
+        return db.query("SELECT * FROM users", callback);
+    },
+    getUserById(user_id, callback) {
+        return db.query("select * from users where id=?", [user_id], callback);
+    },
+    adduser: function(users, callback) {
+        return db.query(
+            "insert into users values(?,?,?,?,?)",
+            [users.user_id, users.username, users.password, users.email, users.admin],
+            callback
+        );
+    },
+    deleteuser: function(user_id, callback) {
+        return db.query(
+            "delete from users where id=?",
+            [user_id],
+            callback
+        );
+    },
+    updateuser: function(user_id, users, callback) {
+        return db.query(
+            "update users set username=?, password=?, email=?, admin=? where user_id=?",
+            [users.username, users.password, users.email, users.admin, user_id],
+            callback
+        );
     }
 };
 
-module.exports =  users;
-
+module.exports = users;
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var users = require("../model/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/:user_id?", function(req, res, next) {
+    if (req.params.user_id) {
+        users.getUserById(req.params.user_id, function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    } else {
+        users.getAllUsers(function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    }
+});
+
+router.post("/", function(req, res, next) {
+    users.adduser(req.body, function(err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body); //or return count for 1 & 0
+        }
+    });
+});
+
+router.delete("/:users_id", function(req, res, next) {
+    users.deleteuser(req.params.users_id, function(err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(count);
+        }
+    });
+});
+
+router.put("/:users_id", function(req, res, next) {
+    users.updateuser(req.params.users_id, req.body, function(err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
 });
 
 module.exports = router;
