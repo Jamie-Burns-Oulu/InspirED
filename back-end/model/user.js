7dd50a47c2814fd2a793a037d3c47a8fe11e99e3
Erik Maresia <maresiaerik@gmail.com>
2018-11-15 14:02:57 +0200

    Mockup, database and usermodel updated

--- back-end/model/user.js
+++ back-end/model/user.js
@@ -6,24 +6,45 @@ const users = {
     getUserById(user_id, callback) {
         return db.query("select * from users where id=?", [user_id], callback);
     },
-    adduser: function(users, callback) {
+    adduser(users, callback) {
         return db.query(
             "insert into users values(?,?,?,?,?)",
             [users.user_id, users.username, users.password, users.email, users.admin],
             callback
         );
     },
-    deleteuser: function(user_id, callback) {
+    deleteuser(user_id, callback) {
         return db.query(
             "delete from users where id=?",
             [user_id],
             callback
         );
     },
-    updateuser: function(user_id, users, callback) {
+    updateuser(user_id, users, callback) {
         return db.query(
             "update users set username=?, password=?, email=?, admin=? where user_id=?",
             [users.username, users.password, users.email, users.admin, user_id],
+            callback
+        );
+    },
+    updatePassword(password, user_id, callback) {
+        return db.query(
+            "update users set password=? where user_id=?",
+            [password, user_id],
+            callback
+        );
+    },
+    updateEmail(email, user_id, callback) {
+        return db.query(
+            "update users set email=? where user_id=?",
+            [email, user_id],
+            callback
+        );
+    },
+    updatePicture(picture, user_id, callback) {
+        return db.query(
+            "update users set picture=? where user_id=?",
+            [picture, user_id],
             callback
         );
     }
