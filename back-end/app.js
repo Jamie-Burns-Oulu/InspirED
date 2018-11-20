8da20e254d17667415795b37dea3e2aab5f74b7d
Florian Brandsma <florianbrandsma@hotmail.nl>
2018-11-20 12:33:03 +0200

    Added user profile route and installation instructions

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var login_register = require('./routes/login_register');
+var user_profile = require('./routes/user_profile');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/login_register', login_register);
+app.use('/user_profile', user_profile);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
