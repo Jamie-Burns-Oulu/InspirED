c8fff2eb5ba88853079520adc3074849e2aa7cec
Jamie Burns <t7buja00@students.oamk.fi>
2018-11-21 13:05:06 +0200

    Completed user settings API

--- back-end/model/user_settings.js
+++ back-end/model/user_settings.js
@@ -1,15 +1,30 @@
 const db = require("../database");
-
 const user_settings = {
-    updateEmail(newEmail, user, callback) {
+
+    updateEmail(info, callback) {
         return db.query(
             "UPDATE user SET email=? WHERE username=?",
-            [newEmail],
-            [user],
+            [info.email, info.username],
             callback
         );
     },
 
-};
+    updatePicture(info, callback) {
+        return db.query(
+            "UPDATE user SET picture=? WHERE username=?",
+            [info.picture, info.username],
+            callback
+        );
+    },
+
+    updatePassword(info, callback) {
+        return db.query(
+            "UPDATE user SET password=? WHERE username=?",
+            [info.password, info.username],
+            callback
+        );
+    }
+
+};    
 
 module.exports = user_settings;
--- back-end/routes/user_settings.js
+++ back-end/routes/user_settings.js
@@ -2,14 +2,34 @@ var express = require("express");
 var router = express.Router();
 var user_settings = require("../model/user_settings");
 
-router.put('/:user', function(req, res, next) {
-   user_settings.updateEmail(req.params.user, req.body, function(err, rows) {
-     if (err) {
-       res.json(err);
-     } else {
-       res.json(rows);
-     }
-   });
- });
+router.put("/email", function(req, res, next) {
+    user_settings.updateEmail(req.body, function(err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+});
+
+router.put("/picture", function(req, res, next) {
+    user_settings.updatePicture(req.body, function(err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+});
+
+router.put("/password", function(req, res, next) {
+    user_settings.updatePassword(req.body, function(err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+});
 
 module.exports = router;
