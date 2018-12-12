971234d2d70c77f9be95bd7ee3bed189bd6b7965
Jamie Burns <t7buja00@students.oamk.fi>
2018-12-12 20:13:03 +0200

    Added styling to quiz landing pages and settings

--- front-end/src/Quiz/Attempted.js
+++ front-end/src/Quiz/Attempted.js
@@ -8,6 +8,8 @@ export default class Completed extends Component {
             window.location = "/Login";
         }
         super();
+        this.mouseEnter = this.mouseEnter.bind(this);
+        this.mouseExit = this.mouseExit.bind(this);
         this.state = {
             attemptedQuizzes: []
         };
@@ -19,51 +21,44 @@ export default class Completed extends Component {
             })
             .then(res => {
                 this.setState({ attemptedQuizzes: res.data });
-                console.log(res.data);
             });
     }
-    result(id) {
-        window.location = "result/" + id;
+    mouseEnter(e, attempted) {
+        const el = e.target;
+        const result = `result/${attempted.id}`;
+        const quiztake = `quiztake/${attempted.id}`;
+        el.innerHTML = `<div class="acn-box-hover">
+                        <i>${attempted.subject_name}, ${
+                        attempted.category_name}</i>
+                        <div><h3>${attempted.name}</h3></div>
+                        <div><a href=${result}>Result</a></div>
+                        <div><a href=${quiztake}>Try again</a></div>
+                        </div>`;
     }
-    take(id) {
-        window.location = "quiztake/" + id;
+    mouseExit(e, attempted) {
+        const el = e.target;
+        el.innerHTML = `<h3>${attempted.name}</h3>`;
     }
 
     render() {
         return (
             <div className="content">
-                <div className="subject-container">
+                <div className="acn-container">
                     <h1>Attempted Quizzes</h1>
-                    <div className="list-container">
-                        <div className="list">
+                    <div className="acn-inner-container">                      
                             {this.state.attemptedQuizzes.map(attempted => (
-                                <div className="box">
-                                    <p>
-                                        <i>
-                                            {attempted.subject_name},{" "}
-                                            {attempted.category_name}
-                                        </i>
-                                    </p>
+                                <div
+                                    className="acn-box"
+                                    onMouseEnter={e => {
+                                        this.mouseEnter(e, attempted);
+                                    }}
+                                    onMouseLeave={e => {
+                                        this.mouseExit(e, attempted);
+                                    }}
+                                >
                                     <h3>{attempted.name}</h3>
-                                    <div
-                                        style={{ cursor: "pointer" }}
-                                        onClick={() => {
-                                            this.take(attempted.quiz_id);
-                                        }}
-                                    >
-                                        Try Again
-                                    </div>
-                                    <div
-                                        style={{ cursor: "pointer" }}
-                                        onClick={() => {
-                                            this.result(attempted.id);
-                                        }}
-                                    >
-                                        View Result
-                                    </div>
                                 </div>
-                            ))}
-                        </div>
+                            ))}                       
                     </div>
                 </div>
             </div>
